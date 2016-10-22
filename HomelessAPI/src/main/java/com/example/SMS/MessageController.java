86d3621311460703fe7150a054f91909ee385d48
eduardo2salas <eduardo2salas@gmail.com>
2016-10-22 16:22:37 -0500

    SMS
    Created Message Creator and test for sending sms messages with twilio's
    sdk

--- HomelessAPI/src/main/java/com/example/SMS/MessageController.java
+++ HomelessAPI/src/main/java/com/example/SMS/MessageController.java
@@ -1,13 +1,15 @@
-package com.example;
+package com.example.SMS;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
+public class MessageController {
 
-    @RequestMapping("/")
+    @RequestMapping("/sms/")
     public String index() {
+
         return "Greetings from Spring Boot!";
     }
 
