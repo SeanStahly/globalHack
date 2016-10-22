70a23356381da1907c6069fc1c0613b2d2aa3bd1
Sean Stahly <seanstahly@gmail.com>
2016-10-22 11:28:43 -0500

    removed maven homeless api

---
+++ HousingAPI/src/main/java/com/example/Client/Client.java
@@ -0,0 +1,35 @@
+package com.example.Client;
+
+/**
+ * Created by sean on 10/22/16.
+ */
+public class Client {
+    private long id;
+    private String name;
+
+    public Client(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Client[id=%d, name = '%s']", id, name);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
