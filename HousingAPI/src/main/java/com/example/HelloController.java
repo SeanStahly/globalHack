9f6e490f329e3253d9e8cd943001b31f5be4828f
Sean Stahly <seanstahly@gmail.com>
2016-10-21 22:45:18 -0500

    first commit

---
+++ HousingAPI/src/main/java/com/example/HelloController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sean on 10/21/16.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
